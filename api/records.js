6432ffc4c3e85b3ea688fef79064ae42ddf01a26
Tom Snyder <tom.snyder627@gmail.com>
2024-06-02 13:53:46 -0500

    Update records.js revert table ID and name to hardcode

--- api/records.js
+++ api/records.js
@@ -1,8 +1,8 @@
 // api/records.js
 
 const airtablePersonalAccessToken = process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN;
-const airtableBaseId = process.env.AIRTABLE_BASE_ID;
-const airtableTableName = process.env.AIRTABLE_TABLE_NAME;
+const airtableBaseId = 'appMq2KDA1JTJLW2f';
+const airtableTableName = 'MainTable';
 
 export default async (req, res) => {
     if (req.method === 'GET') {
